c0d1e55546c1fd66e2cc38dac38e1df3716a378d
widebirb <birbguiruela@gmail.com>
2024-11-14 23:58:03 +0800

    Add project files.

---
+++ midtermPrepa/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using midtermPrepa.Models;
+
+namespace midtermPrepa.Data
+{
+    public class AppDbContext : IdentityDbContext<Users>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ midtermPrepa/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace midtermPrepa.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(40, MinimumLength = 8, ErrorMessage = "The minimum length should be 8 characters.")]
+        [DataType(DataType.Password)]
+        [Compare("ConfirmPassword", ErrorMessage = "Password does not match.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password is required.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
